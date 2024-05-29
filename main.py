5d26ba9fa72e04940503e9f982eb4b190cf53566
CrazyBunQnQ <baobao222222@qq.com>
2024-05-29 17:46:35 +0800

    'v0.0.1'

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 '''
 主模块，用于启动和管理交易系统。
 '''
-from trading_bot import TradingBot
+# from trading_bot import TradingBot
+import time
+
+
 def main():
     bot = TradingBot()
+    print("交易机器人已启动")
     bot.run()
+    while True:
+        # 等待10秒
+        time.sleep(10)
+        bot.run()
+
+
 if __name__ == "__main__":
     main()
